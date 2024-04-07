ad9292c2e318a82feaad93433a846986cb733212
Ruel Paya <ruel.paya@gmail.com>
2024-04-08 05:26:33 +0800

    myLibrary.js

---
+++ src/js/myLibrary.js
@@ -0,0 +1,8 @@
+import regeneratorRuntime from 'regenerator-runtime';
+import trailer from ',/trailers,js';
+import nothingHereUrl from ',,/images/nothingHereUrl,jpg';
+import placeholder from ',/spinner';
+import localStorageApi from ',localStorageApi';
+import cardFilmTpl from ',/templates/card-films,hbs';
+import { renderPagination } from ',/pagination';
+
