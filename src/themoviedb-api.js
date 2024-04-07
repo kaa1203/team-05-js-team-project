bb880a3e27f4f9371f199d4613ad7e3aefc45de3
Ruel Paya <ruel.paya@gmail.com>
2024-04-07 22:42:22 +0800

    Update themoviedb-api.js

--- src/themoviedb-api.js
+++ src/themoviedb-api.js
@@ -10,39 +10,39 @@ export const params = {
         primary_release_year: "",
         page: 1,
         region: "",
-        year: "",
-    }
+        year: ""
+    },
 
     export const fetchpopularArticles = () => {
         const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${params.page}`;
         return fetch(url)
             .then(response => response.json())
             .then(({ results }) => results);
-    };
+    },
     
     export const fetchSearchArticles = () => {
         const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${params.page}&query=${params.query}`;
         return fetch(url)
             .then(response => response.json())
             .then(({ results }) => results);
-    };
+    },
     
     export const fetchpopularArticlesPages = () => {
         const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${params.page}`;
         return fetch(url).then(response => response.json());
-    };
+    },
     
     export const fetchSearchArticlesPages = () => {
         const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${params.page}&query=${params.query}`;
         return fetch(url).then(response => response.json());
-    };
+    },
     
     export const fetchGenres = () => {
         const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
         return fetch(url)
             .then(response => response.json())
             .then(data => data.genres);
-    };
+    },
     
     export const insertGenresToMovieObj = () => {
         return fetchpopularArticles().then(data => {
@@ -54,7 +54,7 @@ export const params = {
                 }));
             });
         });
-    };
+    },
     
     export const insertGenresToSearchObj = () => {
         return fetchSearchArticles().then(data => {
@@ -66,11 +66,11 @@ export const params = {
                 }));
             });
         });
-    };
+    },
     
-    export const getQuery = () => params.query;
-    export const setQuery = newQuery => { params.query = newQuery; };
+    export const getQuery = () => params.query,
+    export const setQuery = newQuery => { params.query = newQuery; },
     
-    export const getPageNumber = () => params.page;
-    export const setPageNumber = newPage => { params.page = newPage; };
+    export const getPageNumber = () => params.page,
+    export const setPageNumber = newPage => { params.page = newPage; },
 }
