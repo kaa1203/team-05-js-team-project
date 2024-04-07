49f62c8e3f5b1bf655a997b6e5c8eb8297d2d24a
Ruel Paya <ruel.paya@gmail.com>
2024-04-08 04:56:18 +0800

    api.js

--- src/themoviedb-api.js
+++ src/themoviedb-api.js
@@ -1,76 +1,86 @@
 export const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "334986b59c344f376defb99ce94fed26";
 
-export const params = {
-    option: {
-        key: API_KEY,
-        query: "",
-        include_adult: false,
-        language: "en-US",
-        primary_release_year: "",
-        page: 1,
-        region: "",
-        year: ""
-    },
+export default class NewApiService {
+    constructor() {
+        this.searchQuery="";
+        this.page = 1;
+    }
 
-    export const fetchpopularArticles = () => {
-        const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${params.page}`;
+    fetchpopularArticles() {
+        const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.page}`;
         return fetch(url)
-            .then(response => response.json())
-            .then(({ results }) => results);
-    },
-    
-    export const fetchSearchArticles = () => {
-        const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${params.page}&query=${params.query}`;
+        .then(response => response.json())
+        .then(({ results}) => {
+            return results;
+        })
+    }
+    fetchSearchArticles() {
+        const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
         return fetch(url)
-            .then(response => response.json())
-            .then(({ results }) => results);
-    },
-    
-    export const fetchpopularArticlesPages = () => {
-        const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${params.page}`;
-        return fetch(url).then(response => response.json());
-    },
-    
-    export const fetchSearchArticlesPages = () => {
-        const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${params.page}&query=${params.query}`;
-        return fetch(url).then(response => response.json());
-    },
-    
-    export const fetchGenres = () => {
-        const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => data.genres);
-    },
-    
-    export const insertGenresToMovieObj = () => {
-        return fetchpopularArticles().then(data => {
-            return fetchGenres().then(genresList => {
-                return data.map(movie => ({
-                    ...movie,
-                    release_date: movie.release_date.split('-')[0],
-                    genres: movie.genre_ids.map(id => genresList.find(el => el.id === id)),
-                }));
-            });
-        });
-    },
-    
-    export const insertGenresToSearchObj = () => {
-        return fetchSearchArticles().then(data => {
-            return fetchGenres().then(genresList => {
-                return data.map(movie => ({
-                    ...movie,
-                    release_date: movie.release_date ? movie.release_date.split('-')[0] : 'n/a',
-                    genres: movie.genre_ids ? movie.genre_ids.map(id => genresList.find(el => el.id === id)) : 'n/a',
-                }));
-            });
-        });
-    },
-    
-    export const getQuery = () => params.query,
-    export const setQuery = newQuery => { params.query = newQuery; },
-    
-    export const getPageNumber = () => params.page,
-    export const setPageNumber = newPage => { params.page = newPage; },
+        .then(response => response.json())
+        .then(({ results}) => {
+            return results;
+    });
 }
+fetchpopularArticlesPages() {
+    const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.page}`;
+    return fetch(url).then(response => response.json());
+}
+fetchSearchArticlesPages() {
+    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+    return fetch(url).then(response => response.json());
+}
+fetchGenres() {
+    const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.page}`;
+    return fetch(url)
+    .then(response => response.json())
+    .then( data => {
+        return data.genres;
+    });
+}
+insertGenresToMovieObj() {
+    return this.fetchpopularArticles().then(data => {
+        return this.fetchGenres().then(genresList => {
+            return data.map(movie => ({
+                ...movie,
+                release_date: movie.release_date.split('-')[0],
+                genres:movie.genre_ids
+                .map(id => genresList.filter(el =>el.id === id))
+                .flat(),
+            }));
+        });
+    });
+}
+insertGenresToSearchObj() {
+    return this.fetchSearchArticles().then(data => {
+        return this.fetchGenres().then(genresList => {
+            let release_date;
+            return data.map(movie => ({
+                ...movie,
+                release_date:movie.release_date
+                ? movie.release_date.split('-')[0]
+                : 'n/a',
+                genres:movie.genre_ids
+                ? movie.genre_ids
+                .map(id => genresList.filter(el => el.id ===id))
+                .flat()
+                :'n/a',
+            }));
+        });
+    });
+}
+get query() {
+    return this.searchQuery;
+}
+set query(newQuery) {
+    this.searchQuery =newQuery;
+}
+get pageNumber() {
+    return this.page;
+}
+set pageNumber(newPage) {
+    this.page = newPage;
+}
+}
+  
