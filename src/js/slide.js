b2c11c67edd145ac98126a1877ebdf30d4d3ddd1
Ruel Paya <ruel.paya@gmail.com>
2024-04-08 05:46:07 +0800

    Create slide.js

---
+++ src/js/slide.js
@@ -0,0 +1,34 @@
+import Glide from '@glidejs/glide';
+import filmsCardSliderTpl from '../templates/card-films-slider.hbs';
+import trailer from './trailers.js';
+const sliderContainer = document.querySelector('.js-slider-container');
+renderTrendy();
+
+const glide = new Glide('.glide', {
+  type: 'slider',
+  startAt: 0,
+  perView: 8,
+  autoplay: 2000,
+  hoverpause: true,
+  bound: true,
+});
+
+glide.mount();
+
+function renderTrendy() {
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=d91911ebb88751cf9e5c4b8fdf4412c9`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ results }) => {
+      return results;
+    })
+    .then(renderSliderFilms)
+    .catch(err => {
+      sliderContainer.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
+    });
+}
+
+function renderSliderFilms(articles) {
+  sliderContainer.innerHTML = filmsCardSliderTpl(articles);
+  trailer.createTrailerLink(document.querySelectorAll('.btn-youtube-slider'));
+}
