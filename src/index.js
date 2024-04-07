53b922f503a9b882d8bd761fb046cee48cbd97d3
Ruel Paya <ruel.paya@gmail.com>
2024-04-08 05:10:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-import { BASE_URL, params } from "./themoviedb-api.js";
-const { language, key } = params.option;
-
-// Testing the api
-
-async function fetchTrending() {
-    const res =  await fetch(`${BASE_URL}/trending/movie/day?api_key=${key}&language=${language}`);
-    const data = await res.json();
-    return data;
-}
-
-try {
-    fetchTrending().then(val => console.log(val));
-} catch (e) {
-    console.log(e);
-}
+import './sass/main.scss';
+import './js/slide';
